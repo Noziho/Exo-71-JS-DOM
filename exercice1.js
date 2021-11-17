0a8dbf504cdc12875aeb44e48f9780fc8e3dc8b1
Noah2211 <noah.decroix3@gmail.com>
2021-11-17 20:06:26 +0100

    Exercice done

--- exercice1.js
+++ exercice1.js
@@ -1,6 +1,20 @@
-let monDiv = .createElement('div');
+let monDiv = document.createElement('div');
+monDiv.innerHTML = "Ce div a été ajouté en utilisant javascript";
 
-document.getElementById('viewport').appendChild(monDiv);
+let newDiv2 = document.createElement('div');
+
+newDiv2.style.background = "red";
+newDiv2.innerHTML = "Div dans viewport"
+newDiv2.style.width = "100px";
+newDiv2.style.height = "100px";
+
+let viewport = document.getElementById('viewport')
+
+viewport.appendChild(monDiv);
+viewport.appendChild(newDiv2);
+
+document.getElementById("aSupprimer").remove();
+
 
 
 
